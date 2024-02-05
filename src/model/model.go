f55da17b81c1b3550c2464514c4ae63e4099539d
kevinmtanadi1 <kevin.tanadi@ruparupa.com>
2024-02-05 18:07:40 +0700

    added more stuffs

--- src/model/model.go
+++ src/model/model.go
@@ -1,10 +1,17 @@
 package model
 
 import (
+	"fmt"
+	"log"
+
 	"gorm.io/gorm"
 )
 
-var tables []interface{}
+var tables []Table
+
+type Table interface {
+	TableName() string
+}
 
 func GenerateTableList() {
 	tables = append(tables,
@@ -13,11 +20,32 @@ func GenerateTableList() {
 		&Product{},
 		&ProductAdditionalData{},
 		&ProductRating{},
+		&CartContent{},
+		&MasterProvince{},
+		&MasterCity{},
+		&MasterKelurahan{},
+		&Order{},
+		&Shipment{},
+		&Transaction{},
+		&TransactionDetail{},
+		&UserAddress{},
+		&Voucher{},
+		&Wallet{},
+		&Wishlist{},
 	)
 }
 
 func MigrateTable(db *gorm.DB) {
 	GenerateTableList()
 
-	db.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(tables...)
+	for _, table := range tables {
+		err := db.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(table)
+
+		created := db.Migrator().HasTable(table.TableName())
+		if created && err == nil {
+			log.Println(fmt.Sprintf("Table %s created", table.TableName()))
+		} else {
+			log.Println(fmt.Sprintf("Error creating table %s: %s", table.TableName(), err.Error()))
+		}
+	}
 }
--- src/model/model_user.go
+++ src/model/model_user.go
@@ -3,17 +3,19 @@ package model
 import "time"
 
 type User struct {
-	ID         uint
-	Username   string
-	Email      string
-	Password   string
-	Token      string
-	Salt       string
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
-	IsVerified uint8           `gorm:"default:0"`
-	Stores     []Store         `gorm:"foreignKey:OwnerID"`
-	Ratings    []ProductRating `gorm:"foreignKey:UserID"`
+	ID           uint
+	Username     string
+	Email        string
+	Password     string
+	Token        string
+	Salt         string
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	IsVerified   uint8           `gorm:"default:0"`
+	Stores       []Store         `gorm:"foreignKey:OwnerID"`
+	Ratings      []ProductRating `gorm:"foreignKey:UserID"`
+	Wishlists    []Wishlist      `gorm:"foreignKey:UserID"`
+	CartContents []CartContent   `gorm:"foreignKey:UserID"`
 }
 
 func (User) TableName() string {
